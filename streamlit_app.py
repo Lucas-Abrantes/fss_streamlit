4bfcfe036bd94b665a9c418a46bb0c85b37a890e
Lucas Abrantes Sarmento <lucas_abrantes25@hotmail.com>
2024-10-12 17:24:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('FSS TELECOMUNICATIONS')
 
-st.write('Hello world!')
+st.info('Aplicativo voltado para otimizações envolvendo superfícies seleticas de frequência.')

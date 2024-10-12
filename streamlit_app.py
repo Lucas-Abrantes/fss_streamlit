ae99337b0274211d5385122cb8c6457d76375486
Lucas Abrantes Sarmento <lucas_abrantes25@hotmail.com>
2024-10-12 17:34:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.title('FSS TELECOMUNICATIONS')
 
 st.info('Aplicativo voltado para otimizações envolvendo superfícies seleticas de frequência.')
 
-df_fss = pd.read_csv('data.csv')
+df_fss = pd.read_csv('https://raw.githubusercontent.com/Lucas-Abrantes/fss_streamlit/refs/heads/master/data.csv')

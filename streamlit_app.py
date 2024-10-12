5f0190711b3729837a12e2101718ce939fa8ddf7
Lucas Abrantes Sarmento <lucas_abrantes25@hotmail.com>
2024-10-12 17:42:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,15 @@ st.info('Aplicativo voltado para otimizações envolvendo superfícies seleticas
 
 df_fss = pd.read_csv('https://raw.githubusercontent.com/Lucas-Abrantes/fss_streamlit/refs/heads/master/data.csv')
 
-df_fss
+with st.expander('Data'):
+  st.write('**Raw data')
+  df_fss = pd.read_csv('https://raw.githubusercontent.com/Lucas-Abrantes/fss_streamlit/refs/heads/master/data.csv')
+  df_fss
+
+  st.write('**Inputs**'):
+  X = df.drop(['BW1', 'BW2', 'RF1', 'RF2'], axis=1)   
+  X
+
+  st.write('**Outputs**'):
+  y = df.drop(['h', 'p', 'd1', 'd2', 'w1', 'w2'], axis=1)
+  y
